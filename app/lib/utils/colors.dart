f790eddda63b6e1837c92bd2b683491a150387dc
nicola gentile <nicola.gentile.developer@gmail.com>
2025-06-06 18:56:45 +0200

    add utils In te utils folder are contained some constant values, like color and sizes that  are used multiple times in the program

---
+++ app/lib/utils/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 255, 115, 0); // Orange color
---
+++ app/lib/utils/sizes.dart
@@ -0,0 +1,4 @@
+const double topBarFontSize = 30;
+const double fontSize = 24;
+const double buttonIconSize = 22;
+const double componentsInterspace = 16;
